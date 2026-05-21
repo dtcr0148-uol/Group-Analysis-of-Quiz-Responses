ab64c2833d0a50c2c98b68d5feac6ae636891d71
dtcr0148-uol <dtcr0148@leeds.ac.uk>
2026-05-21 12:52:48 +0000

    Fix made

--- scripts/data_preparation_M2.py
+++ scripts/data_preparation_M2.py
@@ -1,16 +1,10 @@
-def download_answer_files(cloud_url, path_to_data_folder, total_respondents):
-    import urllib.request
+import urllib.request
 
+def download_answer_files(cloud_url, path_to_data_folder, total_respondents):
     for n in range(1, total_respondents + 1):
         url = cloud_url + "/a" + str(n) + ".txt"
         destination = path_to_data_folder + "/answers_respondent_" + str(n) + ".txt"
         urllib.request.urlretrieve(url, destination)
-
-download_answer_files(
-    "https://raw.githubusercontent.com/fc-leeds/MATH1604_2025_2026_data/main",
-    "data",
-    65
-)
 
 def collate_answer_files(data_folder_path):
     output_path = "output/collated_answers.txt"
